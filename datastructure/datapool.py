6fb9d4dbca20068b22851dcfb9012e0e4c16a521
Wei Yu <yuwei2005@gmail.com>
2024-01-02 09:13:12 +0100

    new file:   datastructure/datalake.py renamed:    datastructure/datastructures.py -> datastructure/datapool.py

--- datastructure/datapool.py
+++ datastructure/datapool.py
@@ -1005,41 +1005,7 @@ class DataPool(object):
         return None
 
 
-#%% Data Lake
 
-class DataLake(object):
-    
-    
-    def __init__(self, items):
-        
-        
-        self.objs = items
-        
-        
-    def keys(self):
-        
-        zcount = len(str(len(self.objs))) + 1
-        
-        strfmt = f':0{zcount}'        
-        
-        fmt = 'DataPool_' + '{' + strfmt + '}'
-                
-        def get():
-            
-            for idx, dp in enumerate(self.objs):
-                
-                if dp.name is None:
-                    
-                    yield fmt.format(idx)
-                else:
-                    
-                    yield dp.name
-                
-        return list(get())
-
-    def search(self, patt, ignore_case=True, raise_error=False):
-                
-        pass
 
 
 #%% Main Loop
