f4162f6cf4ed7dbef147f4f6a3cac04b3602c39a
Wei Yu <yuwei2005@gmail.com>
2024-01-03 14:57:47 +0100

    modified:   datastructure/datalake.py add function "collect_dirs"

--- datastructure/datalake.py
+++ datastructure/datalake.py
@@ -1,5 +1,40 @@
+import os
+import warnings
 
+from itertools import chain
+from pathlib import Path
 
+#%%
+
+def collect_files(root, *patts, warn_if_double=True, ignore_double=False):
+    
+
+    if isinstance(root, (list, tuple, set)):
+        root = chain(*root)
+    
+    found = {}
+       
+    for r, ds, fs in os.walk(root):
+        
+        for f in fs:
+        
+            ismatch = any(re.match(patt, f) for patt in patts)
+            
+            if ismatch: 
+                
+                path = (Path(r) / f)
+                
+                if warn_if_double and f in found:
+                    
+                    dirs = '\n\t'.join(found[f])
+                    
+                    warnings.warn(f'"{path}" exists already in:\n{dirs}')
+                    
+                if (f not in found) or (not ignore_double) :
+                    
+                    yield path
+                    
+                found.setdefault(f, []).append(r)
 
 
 class DataLake(object):
