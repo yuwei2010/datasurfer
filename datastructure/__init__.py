eace4deca113748d89e867d84ba15830837e3627
YUW1SI <yuw1si@bosch.com>
2024-03-01 14:07:30 +0100

    modified:   datastructure/__init__.py

--- datastructure/__init__.py
+++ datastructure/__init__.py
@@ -3,10 +3,8 @@
 __author__ = """Wei Yu (PS-EM/ESY2)"""
 __email__ = 'wei.yu2@de.bosch.com'
 
-
-
 from .datapool import Data_Pool
 from .datalake import Data_Lake
 from .lib_objects import *
 
-__all__   = ['Data_Pool', 'Data_Lake']
+__all__ = ['Data_Pool', 'Data_Lake']
