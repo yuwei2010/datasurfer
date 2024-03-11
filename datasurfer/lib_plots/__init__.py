0849df6a81339a417c15d8d97c8b9fc22dfc71d5
Wei Yu <yuwei2005@gmail.com>
2024-03-11 22:25:04 +0100

    modified:   datasurfer/lib_plots/__init__.py

--- datasurfer/lib_plots/__init__.py
+++ datasurfer/lib_plots/__init__.py
@@ -4,13 +4,38 @@ from collections import abc
 from datasurfer.lib_plots.plot_collection import plot_histogram
 
 class Stat_Plots(object):
+    """
+    A class for generating statistical plots.
+    
+    Parameters:
+    - dp: A pandas DataFrame containing the data.
+    """
     
     def __init__(self, dp):
+        """
+        Initialize the Stat_Plots object.
         
+        Parameters:
+        - dp: A pandas DataFrame containing the data.
+        """
         self.dp = dp
         
     def histogram(self, *keys, ax=None, bins=None, **kwargs):
+        """
+        Generate a histogram plot.
         
+        Parameters:
+        - keys: The column names of the data to plot. If keys are strings, the corresponding columns will be used. 
+                If keys are arrays, the arrays will be used directly.
+        - ax: The matplotlib Axes object to plot on. If None, a new figure and axes will be created.
+        - bins: The number of bins to use in the histogram. If None, a default of 10 bins will be used.
+                If bins is an int, it specifies the number of equal-width bins in the given range.
+                If bins is a sequence, it defines the bin edges, including the rightmost edge.
+        - **kwargs: Additional keyword arguments to be passed to the plot_histogram function.
+        
+        Returns:
+        - ax: The matplotlib Axes object containing the histogram plot.
+        """
         if all(isinstance(key, str) for key in keys):
             data = self.dp[keys].dropna().to_numpy().T
         else:
