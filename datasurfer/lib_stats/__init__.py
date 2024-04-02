f856631c10770686bbee03ad3006225b524b5042
yuwei2010 <yuwei2005@gmail.com>
2024-04-02 15:52:28 +0200

    modified:   datasurfer/lib_stats/__init__.py

--- datasurfer/lib_stats/__init__.py
+++ datasurfer/lib_stats/__init__.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 from datasurfer.datautils import arghisto, parse_data
 
 #%%
@@ -30,11 +31,35 @@ class Stats(object):
         """
         return arghisto(val, bins)
     
-    def corr(self, *keys, **kwargs):
-        
+    def corr(self, *keys, **kwargs):   
+        assert len(keys) >= 2, 'At least two keys are required for correlation computation.'
         return self.dp[keys].corr(**kwargs)
     
+    @parse_data
+    def interp_linearND(self, *vals, **kwargs):
+   
+        from scipy.interpolate import LinearNDInterpolator
+
+        assert len(vals) >= 3, 'At least three keys are required for interpolation.'
+        
+        X = np.vstack(vals[:-1]).T
+        y = np.atleast_2d(vals[-1].ravel()).T
+               
+        mask = np.all(~np.isnan(X), axis=1).ravel() & (~np.isnan(y).ravel())
+        
+        X = X[mask]
+        y = y[mask]
+           
+        f = LinearNDInterpolator(X, y)
+  
+        return lambda arr: f(arr).ravel()
     
+    
+            
+            
+            
+            
+            
         
  
         
