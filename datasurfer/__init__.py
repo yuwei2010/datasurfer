910b39320eb53649a492c6479e49d0cb3c51c8f8
yuwei2010 <yuwei2005@gmail.com>
2024-04-12 23:53:36 +0200

    modified:   .gitignore 	modified:   datasurfer/__init__.py 	new file:   docs/tutorial/01_quick_start.ipynb

--- datasurfer/__init__.py
+++ datasurfer/__init__.py
@@ -36,7 +36,18 @@ def __getattr__(name):
 
 #%%
 def read_string(s, name, **kwargs):
-    
+    """
+    Reads a string and returns a STRING_OBJECT.
+
+    Args:
+        s (str): The input string.
+        name (str): The name of the string object.
+        **kwargs: Additional keyword arguments.
+
+    Returns:
+        STRING_OBJECT: The created STRING_OBJECT.
+
+    """
     from datasurfer.lib_objects.string_object import STRING_OBJECT
     
     return STRING_OBJECT(s, name, **kwargs)
@@ -44,36 +55,58 @@ def read_string(s, name, **kwargs):
 #%%
 
 def to_object(df, name, **kwargs):
-    
+    """
+    Convert a pandas DataFrame to a DATA_OBJECT.
+
+    Parameters:
+        df (pd.DataFrame): The input data as a pandas DataFrame.
+        name (str): The name of the data object.
+        **kwargs: Additional keyword arguments to be passed to the DATA_OBJECT constructor.
+
+    Returns:
+        DATA_OBJECT: The converted data object.
+
+    Raises:
+        AssertionError: If the input data is not a pandas DataFrame.
+    """
     import pandas as pd
-    
     from datasurfer.lib_objects.data_object import DATA_OBJECT
-    
+
     assert isinstance(df, pd.DataFrame), "The input data must be a pandas DataFrame."
-    
+
     return DATA_OBJECT(df, name=name, **kwargs)
 
 #%%
 
 def set_default_interface(ext, path, cls):
-    
+    """
+    Sets the default interface for a given file extension.
+
+    Parameters:
+    - ext (str): The file extension.
+    - path (str): The path to the module containing the interface class.
+    - cls (str): The name of the interface class.
+
+    Returns:
+    - dict: The updated dictionary mapping file extensions to interface information.
+    """
     import importlib
     import json
     from pathlib import Path 
-          
+
     json_file = Path(__file__).parent / 'map_interface.json'
-    
+
     if json_file.is_file():       
         dict_map = json.load(open(json_file, 'r'))
     else:
         dict_map = {}   
-    
+
     c = getattr(importlib.import_module(f'datasurfer.{path}'), cls)
-            
+
     dict_map[ext] = [path, cls]
-    
+
     json.dump(dict_map, open(json_file, 'w'), indent=4)
-    
+
     return dict_map
     
 
