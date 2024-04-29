adeb094ba33936ecbe2c8461187620a82931ecef
yuwei2010 <yuwei2005@gmail.com>
2024-04-29 18:58:52 +0200

    modify multiproc

--- datasurfer/lib_multiproc/__init__.py
+++ datasurfer/lib_multiproc/__init__.py
@@ -6,12 +6,19 @@ import dask.distributed
 #%%
 class MultiProc(object):
     
-    def __init__(self, db=None, n_workers=4, threads_per_worker=2, memory_limit='8GB'):
+    def __init__(self, db=None, n_workers=4, threads_per_worker=1, memory_limit='8GB'):
         
         self.db = db
         self._nworkers = n_workers
         self.threads_per_worker = threads_per_worker
         self.memory_limit = memory_limit
+        
+    def __call__(self, n_workers=None, threads_per_worker=None, memory_limit=None):
+        
+        self.threads_per_worker = threads_per_worker or self.threads_per_worker
+        self.memory_limit = memory_limit or self.memory_limit
+        self.n_workers = n_workers or self.n_workers
+        return self
    
     @property
     def n_workers(self):
@@ -24,8 +31,7 @@ class MultiProc(object):
         
         elif self.n_workers != value:
             self._nworkers = value
-            self.client.close()
-            del self._client
+            self.restart()
             
     
     @property        
