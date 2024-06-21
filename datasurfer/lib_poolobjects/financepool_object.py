28bcad510de1de05effa5620b05c99873de10f48
Yu Wei (EM-EA/ESI2) <Wei.Yu2@de.bosch.com>
2024-06-21 10:42:53 +0200

    add doc to financepool

--- datasurfer/lib_poolobjects/financepool_object.py
+++ datasurfer/lib_poolobjects/financepool_object.py
@@ -9,7 +9,16 @@ from datasurfer.lib_web.yahoofinance_access import YahooFinanceAccess
 WEB_ACCESS = {'yahoo': YahooFinanceAccess}
 #%%
 def strategy_wrapper(func):
-    
+    """
+    A decorator function that wraps a strategy function and returns a StockObject.
+
+    Parameters:
+    - func: The strategy function to be wrapped.
+
+    Returns:
+    - wrapper: The wrapped function that returns a StockObject.
+
+    """
     @wraps(func)
     def wrapper(obj, **kwargs):
         
@@ -28,24 +37,74 @@ def strategy_wrapper(func):
 #%%
 
 class StockObject(FinanceObject):
-    
-    
+    """
+    Represents a stock object.
+
+    Attributes:
+        - symbol: The symbol of the stock.
+        - freq: The frequency of the stock data.
+        - days: The number of days of data to retrieve.
+        - start: The start date of the data.
+        - end: The end date of the data.
+        - config: The configuration for retrieving the data.
+        - access: The web access method to use for retrieving the data.
+
+    Methods:
+        - from_web: Retrieves stock data from the web.
+        - backtesting: Performs backtesting on the stock data.
+        - date2index: Sets the date column as the index of the stock data.
+        - portfolio: Retrieves the latest portfolio value.
+        - plot_operation: Plots the buy and sell operations on a graph.
+    """
+
     @classmethod
     def from_web(cls, symbol, freq, days=None, start=None, 
                    end=None, config=None, access='yahoo'):       
+        """
+        Retrieves stock data from the web.
+
+        Args:
+            - symbol: The symbol of the stock.
+            - freq: The frequency of the stock data.
+            - days: The number of days of data to retrieve.
+            - start: The start date of the data.
+            - end: The end date of the data.
+            - config: The configuration for retrieving the data.
+            - access: The web access method to use for retrieving the data.
+
+        Returns:
+            A new StockObject instance with the retrieved stock data.
+        """
         web_engine = WEB_ACCESS[access]       
         obj = web_engine(symbol, freq, days=days, start=start, end=end, config=config)        
         new_obj = cls.from_other(obj)        
         return new_obj
         
     def backtesting(self, func, **kwargs):  
-                   
+        """
+        Performs backtesting on the stock data.
+
+        Args:
+            - func: The backtesting strategy function to apply.
+            - kwargs: Additional keyword arguments to pass to the strategy function.
+
+        Returns:
+            The result of the backtesting strategy function.
+        """
         func_ = strategy_wrapper(func)
         
         return func_(self, **kwargs)
     
     def date2index(self, name):
-        
+        """
+        Sets the date column as the index of the stock data.
+
+        Args:
+            - name: The name of the date column.
+
+        Returns:
+            The StockObject instance with the date column set as the index.
+        """
         self.df.sort_values(by=name, inplace=True)
         self.df.reset_index(drop=True, inplace=True)
         self.df.set_index(name, inplace=True, drop=False)
@@ -54,11 +113,30 @@ class StockObject(FinanceObject):
         return self
     
     def portfolio(self, by):
-        
+        """
+        Retrieves the latest portfolio value.
+
+        Args:
+            - by: The column to use for calculating the portfolio value.
+
+        Returns:
+            The latest portfolio value.
+        """
         return self.df[by].dropna().iloc[-1]
     
     def plot_operation(self, date='trade_date', base='close', share='shares', **kwargs):
-        
+        """
+        Plots the buy and sell operations on a graph.
+
+        Args:
+            - date: The column to use as the x-axis for the graph.
+            - base: The column to use as the y-axis for the graph.
+            - share: The column to use for calculating the change in shares.
+            - kwargs: Additional keyword arguments to pass to the plot function.
+
+        Returns:
+            The matplotlib Axes object containing the plotted graph.
+        """
         chg_shares = self[share].diff()
         buys = self[base].copy()
         buys[chg_shares <= 0] = np.nan
@@ -79,15 +157,49 @@ class StockObject(FinanceObject):
 #%%
 
 class StockPool(DataPool):
-    
+    """
+    A class representing a pool of stock objects.
+
+    Parameters:
+    - path (str): The path to the data.
+    - config (dict): Configuration options for the pool (optional).
+    - name (str): The name of the pool (optional).
+    - comment (str): Additional comments about the pool (optional).
+    - **kwargs: Additional keyword arguments.
+
+    Attributes:
+    - path (str): The path to the data.
+    - config (dict): Configuration options for the pool.
+    - name (str): The name of the pool.
+    - comment (str): Additional comments about the pool.
+    - objs (list): List of stock objects in the pool.
+
+    Methods:
+    - from_web: Create a stock pool by downloading data from the web.
+    - portfolio: Calculate the portfolio for the stock pool.
+    - backtesting: Perform backtesting on the stock pool.
+    - mlp_backtesting: Perform backtesting using a multi-layer perceptron (MLP) on the stock pool.
+    """
+
     def __init__(self, path, config=None, name=None, comment=None, **kwargs):
-        
         super().__init__(path, interface=StockObject, config=config, name=name, comment=comment, **kwargs)
-        
+
     @classmethod
     def from_web(cls, symbols, pause=5, **kwargs):
+        """
+        Create a stock pool by downloading data from the web.
+
+        Parameters:
+        - symbols (list): List of stock symbols to download.
+        - pause (int): Pause time between each download (default: 5 seconds).
+        - **kwargs: Additional keyword arguments.
+
+        Returns:
+        - StockPool: A stock pool object containing the downloaded data.
+        """
         import time
-        from tqdm import tqdm 
+        from tqdm import tqdm
+
         def get():
             pbar = tqdm(symbols)
             for symbol in pbar:
@@ -95,34 +207,59 @@ class StockPool(DataPool):
                 pbar.set_description(f'{msg}')
                 yield StockObject.from_web(symbol, **kwargs)
                 time.sleep(pause)
-                
+
         return cls(list(get()))
-                    
+
     def portfolio(self, by):
-        
+        """
+        Calculate the portfolio for the stock pool.
+
+        Parameters:
+        - by (str): The method to calculate the portfolio.
+
+        Returns:
+        - pd.Series: A pandas Series object representing the portfolio.
+        """
         return pd.Series(self.map(lambda x: x.portfolio(by)), name=self.name, index=self.names())
-        
-        
+
     def backtesting(self, func, pbar=True, **kwargs):
-        
+        """
+        Perform backtesting on the stock pool.
+
+        Parameters:
+        - func (function): The backtesting function to apply.
+        - pbar (bool): Whether to show a progress bar (default: True).
+        - **kwargs: Additional keyword arguments.
+
+        Returns:
+        - StockPool: A stock pool object containing the backtesting results.
+        """
         name = kwargs.get('name', None) or func.__name__
-        
-        comment = kwargs.pop('comment', None) 
-                              
+        comment = kwargs.pop('comment', None)
+
         @show_pool_progress(f'Backtesting {bcolors.OKGREEN}{bcolors.BOLD}{name}{bcolors.ENDC}', show=pbar)
-        def get(self):           
-            for obj in self.objs:                                           
+        def get(self):
+            for obj in self.objs:
                 yield obj.backtesting(func, **kwargs)
-        
+
         return StockPool(list(get(self)), name=name, comment=comment)
-    
-    
+
     def mlp_backtesting(self, func, **kwargs):
-        comment = kwargs.pop('comment', None) 
+        """
+        Perform backtesting using a multi-layer perceptron (MLP) on the stock pool.
+
+        Parameters:
+        - func (function): The backtesting function to apply.
+        - **kwargs: Additional keyword arguments.
+
+        Returns:
+        - StockPool: A stock pool object containing the backtesting results.
+        """
+        comment = kwargs.pop('comment', None)
         name = kwargs.get('name', None) or func.__name__
         func_ = strategy_wrapper(func)
-        fun = lambda obj: func_(obj, **kwargs)  
-        
+        fun = lambda obj: func_(obj, **kwargs)
+
         return StockPool(self.mlp.map(fun), name=name, comment=comment)
     
 
--- datasurfer/lib_web/yahoofinance_access.py
+++ datasurfer/lib_web/yahoofinance_access.py
@@ -23,13 +23,41 @@ FREQ_ARR = np.array([1, 2, 5, 15, 30, 60, 90, 60, 60*24, 60*24*5, 60*24*7,
 
 #%%
 class YahooFinanceAccess(DataInterface):
-    
+    """
+    A class for accessing Yahoo Finance data.
+
+    Parameters:
+    - symbol (str): The symbol of the stock or asset.
+    - freq (str): The frequency of the data (e.g., '1d' for daily, '1wk' for weekly).
+    - days (int): The number of days of data to retrieve (optional).
+    - start (datetime or tuple): The start date of the data (optional).
+    - end (datetime or tuple): The end date of the data (optional).
+    - config (dict): Configuration settings (optional).
+
+    Attributes:
+    - url (str): The URL for accessing the Yahoo Finance data.
+    - name (str): The symbol of the stock or asset.
+    - response (Response): The HTTP response object.
+    - comment (dict): Additional information about the data.
+    - data (dict): The raw data returned from the API.
+    """
+
     def __init__(self, symbol, freq, days=None, start=None, end=None, config=None):
-        
+        """
+        Initialize the YahooFinanceAccess object.
+
+        Args:
+        - symbol (str): The symbol of the stock or asset.
+        - freq (str): The frequency of the data (e.g., '1d' for daily, '1wk' for weekly).
+        - days (int): The number of days of data to retrieve (optional).
+        - start (datetime or tuple): The start date of the data (optional).
+        - end (datetime or tuple): The end date of the data (optional).
+        - config (dict): Configuration settings (optional).
+        """
+
         super().__init__(path=None, name=symbol, config=config)
                        
         if start is None:
-            
             end = datetime.datetime.now() if end is None else end                
             end = (end if isinstance(end, datetime.datetime) 
                     else datetime.datetime(*end))                
@@ -56,33 +84,59 @@ class YahooFinanceAccess(DataInterface):
         
     @property
     def name(self):
+        """
+        Get the symbol of the stock or asset.
+
+        Returns:
+        - str: The symbol of the stock or asset.
+        """
         return self.data['meta']['symbol']
         
     @property
     def response(self):
-        
+        """
+        Get the HTTP response object.
+
+        Returns:
+        - Response: The HTTP response object.
+        """
         if not hasattr(self, '_response'):
             self._response = requests.get(self.url, headers = {'User-agent': 'your bot 0.1'})
             assert self._response.status_code == 200, f'Request failed, status code: {self._response.status_code}'
-            
             
         return self._response
     
     @property
     def comment(self):       
+        """
+        Get additional information about the data.
+
+        Returns:
+        - dict: Additional information about the data.
+        """
         return self.response.json()['chart']['result'][0]['meta']
     
 
     @property
     def data(self):
-                
+        """
+        Get the raw data returned from the API.
+
+        Returns:
+        - dict: The raw data returned from the API.
+        """
         data = self.response.json()['chart']['result'][0]
                 
         return data
     
     @translate_config()
     def get_df(self):
-        
+        """
+        Get the data as a pandas DataFrame.
+
+        Returns:
+        - DataFrame: The data as a pandas DataFrame.
+        """
         keys = ['date', 'open', 'high', 'low', 'close', 'volume']
         df = pd.DataFrame({**self.data['indicators']['quote'][0], 
                            **dict(date=pd.to_datetime(self.data['timestamp'], unit='s'))}).dropna()[keys]
